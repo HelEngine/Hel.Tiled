5b1b690cf26f8c93422d7c0600b5d2416b833539
GrimmzlyTM <lorenzo@tortonmind.com>
2021-12-14 21:51:42 -0500

    feat: add base tests for all public classes

--- Test/LoaderTests.cs
+++ Test/LoaderTests.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using FluentAssertions;
+using Hel.Tiled;
 using NUnit.Framework;
 
 namespace Test
@@ -5,10 +8,22 @@ namespace Test
     [TestFixture]
     public class LoaderTests
     {
+        
         [Test]
-        public void PlaceholderTest()
-        {
-            Assert.Pass();
+        [TestCase("./MapFiles/beach_tileset.json")]
+        public void Should_LoadTileset_When_ValidPathProvided(string path) {
+            var tileset = Loader.LoadTileset(path);
+
+            tileset.Columns.Should().Be(36);
+            tileset.Image.Should().Be("beach_tileset.png");
+            tileset.Name.Should().Be("beach_tileset");
         }
+
+        [Test]
+        [TestCase("invalid_path.json")]
+        public void Should_ThrowException_On_LoadTileset_When_InvalidPathProvided(string path) =>
+            Assert.Throws<FileNotFoundException>(() => {
+                Loader.LoadTileset(path);
+            });
     }
 }
---
+++ Test/Utilities/TileHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Hel.Tiled.Utilities;
+using NUnit.Framework;
+
+namespace Test.Utilities {
+    
+    [TestFixture]
+    public class TileHelperTests {
+
+        [Test]
+        [TestCase(458, TileHelper.FlippedHorizontallyFlag, true)]
+        [TestCase(232, TileHelper.FlippedVerticallyFlag, false)]
+        [TestCase(676, (uint) 0x00000000, false)]
+        public void Should_DetectFlippedHorizontally_Flag(int gid, uint flag, bool isFlipped) =>
+            TileHelper.IsFlippedHorizontally(SetFlipFlag(gid, flag)).Should().Be(isFlipped);
+
+        private static int SetFlipFlag(int gid, uint flag) => gid | (int)flag;
+    }
+}
